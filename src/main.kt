e239f0f849df9d85e889728096281a55f7c3e674
Matt Doren <mattdoren62@gmail.com>
2020-11-16 12:15:20 -0600

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,10 @@
 fun main(){
+    //The employees are created here
     var emp1 = Employee("Jane Doe", PositionTitle.ADMINISTRATION, true, 27.00, 1);
     var emp2 = Employee("Larry Jackson", PositionTitle.PRODUCTION, false, 22.00, 2);
     var emp3 = Employee("John Smith", PositionTitle.MAINTENANCE, false, 21.00, 3);
 
+    //The functions display and calculate are called from the Employee class here
     emp1.display()
     emp1.calculate(45)
 
